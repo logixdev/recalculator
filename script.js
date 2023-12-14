20e53ae4372ff1d3df634c53a4432476db492bb4
Michał Kansy <logixdev96@gmail.com>
2023-12-14 20:09:42 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+function calculate(calculatorType) {
+  const commissionField = document.getElementById(`commission${calculatorType}`);
+  const orderValueField = document.getElementById(`orderValue${calculatorType}`);
+  const discountField = document.getElementById(`discount${calculatorType}`);
+
+  const commission = parseFloat(commissionField.value) || 0;
+  const orderValue = parseFloat(orderValueField.value) || 0;
+  const discount = parseFloat(discountField.value) || 0;
+
+  const discountedOrderValue = orderValue - (orderValue * (discount / 100));
+  const calculatedCommission = (discountedOrderValue * (commission / 100)).toFixed(2);
+  const calculatedRevenue = (discountedOrderValue - calculatedCommission).toFixed(2);
+
+  document.getElementById(`calculatedOrderValue${calculatorType}`).textContent = `Wartość zamówienia po zniżce: ${discountedOrderValue.toFixed(2)} PLN`;
+  document.getElementById(`calculatedCommission${calculatorType}`).textContent = `Prowizja do zapłaty: ${calculatedCommission} PLN`;
+  document.getElementById(`calculatedRevenue${calculatorType}`).textContent = `Przychód: ${calculatedRevenue} PLN`;
+
+  const revenueDifference = Math.abs(
+    parseFloat(document.getElementById('calculatedRevenuePortal').textContent.split(' ')[1]) -
+    parseFloat(document.getElementById('calculatedRevenueRestaumatic').textContent.split(' ')[1])
+  ).toFixed(2);
+
+  document.getElementById('revenueDifference').textContent = `${revenueDifference} PLN`;
+}
+
+window.onload = function () {
+  calculate('Portal');
+  calculate('Restaumatic');
+};
